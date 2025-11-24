fca711d5ec5738f8431139436fb68264759ab3ae
drm121918-dev <drm121918@gmail.com>
2025-11-24 18:01:14 +0530

    Delete vars.py

--- vars.py
+++
@@ -1,9 +0,0 @@
-# Don't Remove Credit Tg - @Tushar0125
-# Ask Doubt on telegram @Tushar0125
-
-from os import environ
-
-API_ID = int(environ.get("API_ID", "36925285")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "ef3e2c581370c93287854dc36d78c13c") #Replace with your api hash
-BOT_TOKEN = environ.get("8361069721:AAENqJs0H83lUP4LSM0mFX1Mx3jEtCEQ4zA", "") #Replace with your bot token
-
