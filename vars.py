e7bb21a020d84f17a763de505e26fde8d4d7f186
drm121918-dev <drm121918@gmail.com>
2025-11-24 18:11:59 +0530

    Update API credentials and MongoDB connection string

--- vars.py
+++ vars.py
@@ -3,13 +3,13 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22946135")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "93e1b0c387cabe34a3ccfa1724ae8527") #Replace with your api hash
+API_ID = int(environ.get("API_ID", "36925285")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "ef3e2c581370c93287854dc36d78c13c") #Replace with your api hash
 BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
 
 # Database Configuration for MongoDB
 # Example: "mongodb://user:password@host:port/dbname"
-# For MongoDB Atlas: "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/<dbname>?retryWrites=true&w=majority"
+# For MongoDB Atlas: "mongodb+srv://<Advance_bot>:<abc*1234>@<drmbot12345>.mongodb.net/<drmbot12345>?retryWrites=true&w=majority"
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb://localhost:27017/your_bot_db")
 # Replace "your_bot_db" with your desired database name.
 # For local testing, you might use "mongodb://localhost:27017/your_bot_db"
